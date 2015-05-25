8fd6d0b9a5e6ff58465c7cd7b6fdb557c9735b1b
Anthony Ma <tuogex@yahoo.com>
2015-05-25 18:43:10 -0500

    Update class names and demo adding Component to Controller

--- demos/windows.lua
+++ demos/windows.lua
@@ -1,12 +1,12 @@
 os.loadAPI("txUI")
-txUI.UIManager.spaceColor = colors.lightBlue
+txUI.Controller.spaceColor = colors.lightBlue
 local w, h = term.getSize()
 local window = txUI.Window:new({w = w - 5; h = h - 5; draggable = true; hasShadow = true; bgColor = colors.white; tlColor = colors.blue; shadowColor = colors.gray;})
-txUI.UIManager:addWindow(window)
-txUI.UIManager:setVisibleWindow(window)
+txUI.Controller:addWindow(window)
+txUI.Controller:setVisibleWindow(window)
 window:setTitleLabel(txUI.Label:new({text = "txUI Window"; bgColor = window.tlColor; textColor = colors.white; w = window.w; x = window.x; textAlign = "right";}))
 
-window:addComponent(txUI.Button:new({x = 1; y = 1; w = 1; h = 1; action = (function(self) txUI.UIManager:exit(); end); textColor = colors.red; bgColor = window.tlColor; text = "X";}))
-window:addComponent(txUI.Label:new({x = 1; y = 7; w = window.w; text = "txUI Windows!"; textColor = colors.lightGray;}))
+window:addComponent(txUI.Button:new({x = 1; y = 1; w = 1; h = 1; action = (function(self) txUI.Controller:exit(); end); textColor = colors.red; bgColor = window.tlColor; text = "X";}))
+txUI.Controller:addComponent(txUI.Label:new({x = 1; y = 9; w = w; text = "txUI Windows!"; textColor = colors.white; bgColor = colors.lightBlue;}))
 
-txUI.UIManager:startUpdateCycle()
+txUI.Controller:startUpdateCycle()
